fca1bd8d2e64109c2027d70a54d0550c944ee56f
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-01-29 10:36:35 -0600

    Example Abstract test PART. 2

--- src/test/kotlin/structural_patterns/Adapter.kt
+++ src/test/kotlin/structural_patterns/Adapter.kt
@@ -1,5 +1,8 @@
 package structural_patterns
 
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
 // 3rd party functionality
 
 data class DisplayDataType(
@@ -24,9 +27,82 @@ class DatabaseGenerator {
     fun generatorData(): List<DatabaseData> {
         val list = arrayListOf<DatabaseData>()
         list.add(DatabaseData(position = 2, amount = 2))
-        list.add(DatabaseData(position =  3, amount = 7))
+        list.add(DatabaseData(position = 3, amount = 7))
         list.add(DatabaseData(position = 4, amount = 23))
         return list
+    }
+}
+
+interface DatabaseConverter {
+    fun convertData(data: List<DatabaseData>): List<DisplayDataType>
+}
+
+class DataDisplayAdapter(val display: DataDisplay) : DatabaseConverter {
+    override fun convertData(data: List<DatabaseData>): List<DisplayDataType> {
+        val returnList = arrayListOf<DisplayDataType>()
+        for (datum in data) {
+            val ddt = DisplayDataType(
+                index = datum.position.toFloat(),
+                data = datum.amount.toString()
+            )
+            display.displayData(ddt)
+            returnList.add(ddt)
+        }
+        return returnList
+    }
+}
+
+class AdapterTest {
+    @Test
+    fun adapterListSizeTest() {
+        val generator = DatabaseGenerator()
+        val generatedData = generator.generatorData()
+        val adapter = DataDisplayAdapter(display = DataDisplay())
+        val convertData = adapter.convertData(generatedData)
+
+        assertEquals(
+            actual = convertData.size,
+            expected = 3
+        )
+    }
+
+    @Test
+    fun adapterItem0Test() {
+        val generator = DatabaseGenerator()
+        val generatedData = generator.generatorData()
+        val adapter = DataDisplayAdapter(DataDisplay())
+        val convertData = adapter.convertData(generatedData)
+
+        assertEquals(
+            actual = convertData[0],
+            expected = DisplayDataType(2F, "2")
+        )
+    }
+
+    @Test
+    fun adapterItem1Test() {
+        val generator = DatabaseGenerator()
+        val generatedData = generator.generatorData()
+        val adapter = DataDisplayAdapter(DataDisplay())
+        val convertData = adapter.convertData(generatedData)
+
+        assertEquals(
+            actual = convertData[1],
+            expected = DisplayDataType(3F, "7")
+        )
+    }
+
+    @Test
+    fun adapterItem2Test() {
+        val generator = DatabaseGenerator()
+        val generatedData = generator.generatorData()
+        val adapter = DataDisplayAdapter(DataDisplay())
+        val convertData = adapter.convertData(generatedData)
+
+        assertEquals(
+            actual = convertData[2],
+            expected = DisplayDataType(4F, "23")
+        )
     }
 }
 
